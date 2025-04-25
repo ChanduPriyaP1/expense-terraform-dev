98831f8dd915dff4510656f3b0c2c923d4bcdc24
Siva <sivakumarpulis@gmail.com>
2025-04-25 10:59:08 +0530

    jenkins file

--- 01-vpc/vpc-test.tf
+++ 01-vpc/vpc-test.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-  source = "../../..terraform-aws-vpc"
-  #source = "https://github.com/ChanduPriyaP1/terraform-aws-vps.git?ref=main"
+  #source = "../../..terraform-aws-vpc"
+  source = "https://github.com/ChanduPriyaP1/terraform-aws-vps.git?ref=main"
   project_name = var.project_name
   common_tags = var.common_tags
   public_subnet_cidrs = var.public_subnet_cidrs
@@ -8,3 +8,4 @@ module "vpc" {
   database_subnet_cidrs = var.database_subnet_cidrs
   is_peering_required = var.is_peering_required
 }
+# https://github.com/daws-78s/terraform-aws-vpc.git?ref=main
